332b6ace40f884f92dd6a6af6a2dd11219e3b68d
Ingenious-c0der <sagarabhyankar18@gmail.com>
2024-06-07 17:05:45 +0530

    use net

--- test.js
+++ test.js
@@ -1,26 +1,17 @@
 const crypto = require("crypto");
 const Swarm = require("discovery-swarm");
 const defaults = require("dat-swarm-defaults");
-const getPort = require("get-port");
+const net = require("net");
 const readline = require("readline");
 
-/**
- * Here we will save our TCP peer connections
- * using the peer id as key: { peer_id: TCP_Connection }
- */
 const peers = {};
-// Counter for connections, used for identify connections
 let connSeq = 0;
 
-// Peer Identity, a random hash for identify your peer
 const myId = crypto.randomBytes(32);
-console.log("Your identity: " + myId.toString("hex"));
+console.log('Your identity: ' + myId.toString('hex'));
 
-// reference to redline interface
 let rl;
-/**
- * Function for safely call console.log with readline interface active
- */
+
 function log() {
   if (rl) {
     rl.clearLine();
@@ -33,18 +24,13 @@ function log() {
   askUser();
 }
 
-/*
- * Function to get text input from user and send it to other peers
- * Like a chat :)
- */
 const askUser = async () => {
   rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
   });
 
-  rl.question("Send message: ", (message) => {
-    // Broadcast to peers
+  rl.question('Send message: ', message => {
     for (let id in peers) {
       peers[id].conn.write(message);
     }
@@ -54,73 +40,70 @@ const askUser = async () => {
   });
 };
 
-/**
- * Default DNS and DHT servers
- * This servers are used for peer discovery and establishing connection
- */
 const config = defaults({
-  // peer-id
   id: myId,
 });
 
-/**
- * discovery-swarm library establishes a TCP p2p connection and uses
- * discovery-channel library for peer discovery
- */
 const sw = Swarm(config);
 
-(async () => {
-  // Choose a random unused port for listening TCP peer connections
-  const port = await getPort();
-
-  sw.listen(port);
-  console.log("Listening to port: " + port);
-
-  /**
-   * The channel we are connecting to.
-   * Peers should discover other peers in this channel
-   */
-  sw.join("our-fun-channel");
-
-  sw.on("connection", (conn, info) => {
-    // Connection id
-    const seq = connSeq;
-
-    const peerId = info.id.toString("hex");
-    log(`Connected #${seq} to peer: ${peerId}`);
-
-    // Keep alive TCP connection with peer
-    if (info.initiator) {
-      try {
-        conn.setKeepAlive(true, 600);
-      } catch (exception) {
-        log("exception", exception);
-      }
-    }
-
-    conn.on("data", (data) => {
-      // Here we handle incomming messages
-      log("Received Message from peer " + peerId, "----> " + data.toString());
+const getAvailablePort = () => {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+    server.listen(0, () => {
+      const port = server.address().port;
+      server.close(() => {
+        resolve(port);
+      });
     });
-
-    conn.on("close", () => {
-      // Here we handle peer disconnection
-      log(`Connection ${seq} closed, peer id: ${peerId}`);
-      // If the closing connection is the last connection with the peer, removes the peer
-      if (peers[peerId].seq === seq) {
-        delete peers[peerId];
-      }
-    });
-
-    // Save the connection
-    if (!peers[peerId]) {
-      peers[peerId] = {};
-    }
-    peers[peerId].conn = conn;
-    peers[peerId].seq = seq;
-    connSeq++;
+    server.on('error', reject);
   });
+};
 
-  // Read user message from command line
-  askUser();
+(async () => {
+  try {
+    const port = await getAvailablePort();
+    sw.listen(port);
+    console.log('Listening to port: ' + port);
+
+    sw.join('our-fun-channel');
+
+    sw.on('connection', (conn, info) => {
+      const seq = connSeq;
+      const peerId = info.id.toString('hex');
+      log(`Connected #${seq} to peer: ${peerId}`);
+
+      if (info.initiator) {
+        try {
+          conn.setKeepAlive(true, 600);
+        } catch (exception) {
+          log('exception', exception);
+        }
+      }
+
+      conn.on('data', data => {
+        log(
+          'Received Message from peer ' + peerId,
+          '----> ' + data.toString()
+        );
+      });
+
+      conn.on('close', () => {
+        log(`Connection ${seq} closed, peer id: ${peerId}`);
+        if (peers[peerId].seq === seq) {
+          delete peers[peerId];
+        }
+      });
+
+      if (!peers[peerId]) {
+        peers[peerId] = {};
+      }
+      peers[peerId].conn = conn;
+      peers[peerId].seq = seq;
+      connSeq++;
+    });
+
+    askUser();
+  } catch (err) {
+    console.error('Failed to get available port:', err);
+  }
 })();
